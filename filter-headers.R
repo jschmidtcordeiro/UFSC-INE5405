8413c0bc296e5a2dd2ceb8ce9b727650dc85c32f
Joao Cordeiro <jschmidtcordeiro@gmail.com>
2024-09-22 17:08:52 -0300

    feat: add ID_SERVIDOR and rename some variables

--- filter-headers.R
+++ filter-headers.R
@@ -1,6 +1,7 @@
 source("functions/get-headers.R")
 source("functions/select-headers.R")
 source("functions/retain-rows-with-string.R")
+source("functions/update-csv-headers.R")
 
 # Example usage
 # Path to your file
@@ -13,6 +14,7 @@ output_file_path <- "output/combined_result.csv"
 # List of headers to select (adjust this list as needed)
 headers_to_select <- c(
   "NOME",
+  "Id_SERVIDOR_PORTAL.x",
   "TIPO_VINCULO",
   "DESCRICAO_CARGO",
   # "UORG_LOTACAO",
@@ -29,10 +31,10 @@ headers_to_select <- c(
 )  # Replace with your actual headers
 
 # Create a new file with only the selected headers
-# select_headers(file_path, headers_to_select, output_file_path)
+select_headers(file_path, headers_to_select, output_file_path)
 
 # Get and display headers
-# headers <- get_headers(file_path)
+headers <- get_headers(file_path)
 
 data <- read.csv(file_path, header = TRUE, sep = ",", fileEncoding = "latin1")
 
@@ -43,3 +45,13 @@ cleaned_data <- retain_rows_with_string(data, "ORG_EXERCICIO", "Universidade Fed
 write.csv(cleaned_data, "output/filtered_ufsc.csv", row.names = FALSE, fileEncoding = "latin1")
 
 print("Filtered CSV created: filtered_result.csv")
+
+# Update the headers in the filtered CSV
+header_updates <- c(
+  "Id_SERVIDOR_PORTAL.x" = "ID_SERVIDOR_PORTAL", 
+  "REMUNERAÃ.Ã.O.BÃ.SICA.BRUTA..R.." = "REMUNERACAO_BASICA_BRUTA",
+  "REMUNERAÃ.Ã.O.APÃ.S.DEDUÃ.Ã.ES.OBRIGATÃ.RIAS..R.." = "REMUNERACAO_APOS_DEDUCOES",
+  "TOTAL.DE.VERBAS.INDENIZATÃ.RIAS..R....." = "TOTAL_DE_VERBAS_INDENIZATORIAS"
+)
+
+update_csv_headers("output/filtered_ufsc.csv", header_updates)
